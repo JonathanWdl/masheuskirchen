ae230681d7a8adae53d1aa53a9658790f69727e8
Jonathan Wedel <jonathanwdl@pop-os.localdomain>
2019-09-03 21:49:10 +0200

    Extended bash script to clone named extensions after typo3 is installed

--- container/bin/composer-install.sh
+++ container/bin/composer-install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # get php container id
-container=$(docker ps -qf "name=masheuskirchen_php_1")
+cPHP=$(docker ps -qf "name=masheuskirchen_php_1")
 
 # run composer install as non root user
-docker exec -it $container composer install
+docker exec -it $cPHP composer install
+
+# import database after installation
+echo import database ...
+cDB=$(docker ps -qf "name=masheuskirchen_db_1")
+docker exec $cDB mysqldump -u typo3 --password=typo3 typo3 > $(dirname "$0")/../../data/typo3_backup.sql
+
+# clone wdl_specials repository
+echo clone wdl_specials from GitHhub ...
+docker exec -it $cPHP cd wwwroot/typo3conf/ext/ && \
+docker exec -it $cPHP git clone https://github.com/JonathanWdl/wdl_specials.git
