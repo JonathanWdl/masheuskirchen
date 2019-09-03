260f3c289d70744da6ecb3d8e516613d59352d15
Jonathan Wedel <jonathan.wedel@webrunners.de>
2019-09-03 14:12:26 +0200

    Resetted changes on docker files for mac
    Now the IDs are passed again and the user www-data is used in PHP container.

--- container/bin/run.sh
+++ container/bin/run.sh
@@ -3,6 +3,9 @@ set -eu
 
 cd "$(dirname "$0")/../"
 
+export cUID=$(id -u ${USER})
+export gUID=$(id -g ${USER})
+
 set -x
 
 COMPOSE_PROJECT_NAME=masheuskirchen COMPOSE_FILE=docker-compose.yml exec docker-compose ${@:-up -d}
