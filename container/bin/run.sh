1675eabe5548d315d2a97687f8b36986f56c18ef
Jonathan Wedel <jonathan.wedel@webrunners.de>
2019-09-03 12:20:51 +0200

    Removed id export in run script

--- container/bin/run.sh
+++ container/bin/run.sh
@@ -3,10 +3,6 @@ set -eu
 
 cd "$(dirname "$0")/../"
 
-# Export dynamically the host user id and group-id
-export cUID=$(id -u ${USER})
-export cGID=$(id -g ${USER})
-
 set -x
 
 COMPOSE_PROJECT_NAME=masheuskirchen COMPOSE_FILE=docker-compose.yml exec docker-compose ${@:-up -d}
