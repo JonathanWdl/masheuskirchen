3a4c865f1bcb3a043cda0595c8bad0fd98225ee3
Jonathan Wedel <jonathan.wedel@webrunners.de>
2019-09-04 17:11:48 +0200

    Updated bash script for database import

--- container/bin/import_db.sh
+++ container/bin/import_db.sh
@@ -8,7 +8,9 @@ file=$(dirname "$0")/../../data/typo3_backup.sql
 
 if [ -f $file ]; then
     # import database
+    echo "import database ..."
     cat $file | docker exec -i $container mysql -u typo3 --password=typo3 typo3
+    echo "import done."
 else
     echo "Database Backup does not exist!"
 fi
